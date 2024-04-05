92a126445ec1cff1ef95a2210421e163d846277d
SRINADH <115113319+Srinadhch07@users.noreply.github.com>
2024-04-05 12:17:16 +0530

    Update TreeTraversalTechnique(Using C and  C++).cpp

--- TreeTraversalTechnique(Using C and  C++).cpp
+++ TreeTraversalTechnique(Using C and  C++).cpp
@@ -1,23 +1,60 @@
 //written by SRINADHCH07
-//code: virtual destructors in inheritance 
-
+//code ;tree traversal techniques with c and c++ 
+/*code desrciption :1.checking  class and struct has  same properties*/
 #include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
 using namespace std;
-class A{
+class  n{
 public:
-virtual ~A(){
-cout<<"hi"<<endl;
-}
+int data;
+class n*l;
+class n*r;
 };
-class B:public A
-{public :
- virtual ~B()
-{
-cout<<"hello";
-}};
-int main(){
-B o;
+class  n *nn( int data){
+class n *n=(class n*)malloc(sizeof(class n*));
+n->data=data;
+n->l=NULL;
+n->r=NULL;
+return (n);
 }
+void post(class n*n){
+if(n==NULL)
+return;
+post(n->l);
+post(n->r);
+printf("%d  ",n->data);
+}
+void  ino(class n *n){
+if(n==NULL)
+return;
+ino(n->l);
+printf("%d  ",n->data);
+ino(n->r);
+}
+void pre(class n *n){
+if(n==NULL)
+return;
+printf("%d  ",n->data);
+pre(n->l);
+pre(n->r);
+}
+int main(){
+class  n *r=nn(3);
+r->l=nn(2);
+r->r=nn(5);
+r->l->r=nn(6);
+r->l->l=nn(0);
+r->r->l=nn(8);
+r->l->r->r==nn(7);
+printf("\nPreorder traversal \n");
+pre(r);
+printf("\n\nINorder treaversal \n");
+ino(r);
+printf("\n\nPOst order traversal \n");
+post(r);
+} 
+    
 
-
+#c
 #c++
